17ed220d2214ab47e6072408e253d8ea996e6071
James Deucker <me@bitwisecook.org>
2019-07-02 19:12:12 +0100

    add the start of a completion provider

--- src/extension.ts
+++ src/extension.ts
@@ -15,10 +15,10 @@ export function activate(context: vscode.ExtensionContext) {
 				let dl: vscode.TextLine = document.lineAt(line);
 				let t: string = dl.text;
 				if (t.endsWith(' {')) {
-					tab += tabdepth
+					tab += tabdepth;
 				}
 				if (t.endsWith('}')) {
-					tab -= tabdepth
+					tab -= tabdepth;
 				}
 				if (tab > 0) {
 					console.log('redoing line ' + line);
@@ -35,6 +35,48 @@ export function activate(context: vscode.ExtensionContext) {
 			return edits;
 		}
 	});
+
+	const completionProvider = vscode.languages.registerCompletionItemProvider(
+		'irule-lang',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				console.log('completion');
+				let linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('log ')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem('local0.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local1.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local2.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local3.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local4.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local5.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local6.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('local7.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('kern.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('user.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('mail.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('daemon.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('auth.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('syslog.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('lpr.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('news.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('uucp.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('cron.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('authpriv.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('ftp.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('ntp.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('security.', vscode.CompletionItemKind.EnumMember),
+					new vscode.CompletionItem('console.', vscode.CompletionItemKind.EnumMember),
+				];
+			}
+		},
+		' ' // triggered whenever a '.' is being typed
+	);
+
+	context.subscriptions.push(completionProvider);
 }
 
 // this method is called when your extension is deactivated
