fec59c49f89af18b982792b569b0ad63ed842be5
James Deucker <me@bitwisecook.org>
2019-07-05 17:09:31 +0100

    change to using YAML for tmLanguage

---
+++ src/build.ts
@@ -0,0 +1,23 @@
+import * as converter from 'converter';
+import { createReadStream, createWriteStream, mkdir } from 'fs';
+
+/*
+    Create syntax directory
+*/
+mkdir('./out/syntaxes', (callback) => { return; });
+
+const language = (l: any[], i: number, a: { length: any; }) => {
+    console.info(`${l[1]} (${i + 1}/${a.length})`);
+    const options = { from: 'yml', to: 'plist' };
+    const from = createReadStream(`./syntaxes/${l[0]}.YAML-tmlanguage`);
+    const to = createWriteStream(`./out/syntaxes/${l[0]}.tmlanguage`);
+    const via = converter(options);
+    from.pipe(via).pipe(to);
+};
+
+const syntax = [
+    ['irule', 'F5 iRule'],
+];
+
+console.info('Building Grammar\n');
+syntax.forEach(language);
