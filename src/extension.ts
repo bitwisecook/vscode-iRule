876cb90bcd725ec5c64369852476d6f70fde173a
James Deucker <me@bitwisecook.org>
2019-07-17 19:50:15 +0100

    hack on memfs until it fetches irules from a single lab device

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,10 @@ import * as dochelp from "./documentHelpers";
 import * as format from "./formatProvider";
 import * as complete from "./completionProvider";
 import * as diagnostic from "./diagnosticsProvider";
+import * as fs from './fsProvider';
+import { IcrFS } from './fsProvider';
+import * as request from 'request';
+import * as fetch from 'fetch';
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.languages.registerDocumentFormattingEditProvider("irule-lang", {
@@ -118,6 +122,100 @@ export function activate(context: vscode.ExtensionContext) {
             diagnostic.updateDiagnostics(changeEvent.document, collection)
         )
     );
+
+
+    console.log('IcrFS says "Hello"');
+
+    const icrFs = new IcrFS();
+    context.subscriptions.push(vscode.workspace.registerFileSystemProvider('icrfs', icrFs, { isCaseSensitive: true }));
+    let initialized = false;
+
+    context.subscriptions.push(vscode.commands.registerCommand('icrfs.reset', _ => {
+        console.log('execute reset');
+        for (const [name] of icrFs.readDirectory(vscode.Uri.parse('icrfs:/'))) {
+            icrFs.delete(vscode.Uri.parse(`icrfs:/${name}`));
+        }
+        initialized = false;
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('icrfs.addFile', _ => {
+        console.log('execute addFile');
+        if (initialized) {
+            icrFs.writeFile(vscode.Uri.parse(`icrfs:/file.txt`), Buffer.from('foo'), { create: true, overwrite: true });
+        }
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('icrfs.deleteFile', _ => {
+        console.log('execute deleteFile');
+        if (initialized) {
+            icrFs.delete(vscode.Uri.parse('icrfs:/file.txt'));
+        }
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('icrfs.init', _ => {
+        console.log('execute init');
+        if (initialized) {
+            return;
+        }
+        initialized = true;
+
+        var options = {
+            method: 'GET',
+            url: 'https://xxx/mgmt/tm/sys/folder',
+            headers:
+            {
+                Connection: 'keep-alive',
+                Host: 'xxx',
+                'Cache-Control': 'no-cache',
+                Accept: '*/*',
+                Authorization: 'Basic xxxxxx'
+            }
+        };
+
+        let dirs: fs.Directory[] = [];
+        request(options.url, options, function (error, response, body) {
+            if (error) {
+                throw new Error(error);
+            }
+            let json = JSON.parse(body);
+            let items = json['items'];
+            items.forEach((item: { [x: string]: string; }) => {
+                if (item.fullPath === '/') {
+                    return;
+                }
+                try {
+                    icrFs.createDirectory(vscode.Uri.parse('icrfs://' + item.fullPath + '/'));
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+            console.log('fetched ' + dirs.length + ' dirs');
+            options.url = 'https://xxx/mgmt/tm/ltm/rule';
+            request(options.url, options, function (error, response, body) {
+                if (error) {
+                    throw new Error(error);
+                }
+                let json = JSON.parse(body);
+                let items = json['items'];
+                items.forEach((item: { [x: string]: string; }) => {
+                    if (item.fullPath === '/') {
+                        return;
+                    }
+                    try {
+                        icrFs.writeFile(vscode.Uri.parse('icrfs://' + item.fullPath + '.irul'), Buffer.from(item.apiAnonymous), { create: true, overwrite: true });
+                    } catch (error) {
+                        console.log(error);
+                    }
+                });
+                console.log('fetched ' + dirs.length + ' dirs');
+            });
+        });
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('icrfs.workspaceInit', _ => {
+        console.log('execute workspaceInit');
+        vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.parse('icrfs:/'), name: "IcrFS - Sample" });
+    }));
 }
 
 // this method is called when your extension is deactivated
