57face0178dd28829c647dc572c9228fbf074956
James Deucker <me@bitwisecook.org>
2019-12-04 21:15:44 +0200

    fix syntax filename for case sensitive filesystems and clean up .vscodeignore

--- src/build.ts
+++ src/build.ts
@@ -9,8 +9,8 @@ mkdir('./out/syntaxes', (callback) => { return; });
 const language = (l: any[], i: number, a: { length: any; }) => {
     console.info(`${l[1]} (${i + 1}/${a.length})`);
     const options = { from: 'yml', to: 'plist' };
-    const from = createReadStream(`./syntaxes/${l[0]}.YAML-tmlanguage`);
-    const to = createWriteStream(`./out/syntaxes/${l[0]}.tmlanguage`);
+    const from = createReadStream(`./syntaxes/${l[0]}.YAML-tmLanguage`);
+    const to = createWriteStream(`./out/syntaxes/${l[0]}.tmLanguage`);
     const via = converter(options);
     from.pipe(via).pipe(to);
 };
