67be49caa6c594edfcaf87fc52727f28530c0259
James Deucker <me@bitwisecook.org>
2020-07-08 11:32:53 +0100

    fix arg injection diagnostic

--- src/diagnosticsProvider.ts
+++ src/diagnosticsProvider.ts
@@ -71,12 +71,12 @@ export function updateDiagnostics(
                 line
             );
             if (vscode.workspace.getConfiguration().get('conf.irule-lang.diag.arginject.enable') && match) {
-                const idx = line.indexOf((match[2] ? match[2] : match[4]));
+                const idx = line.indexOf((match[1] ? match[1] : match[3]));
                 diags.push(
                     {
                         code: "",
                         message:
-                            `\`${match[2] || match[4]}\` permits argument injection, add \`--\` to terminate options`,
+                            `\`${match[1] || match[3]}\` permits argument injection, add \`--\` to terminate options`,
                         range: new vscode.Range(
                             new vscode.Position(lineNum, idx),
                             new vscode.Position(lineNum, line.length)
